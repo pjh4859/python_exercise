13c8e06da524f82c12784e647f6ba26134171b37
JAEHEE PARK <pjh4859@naver.com>
2020-02-01 12:43:40 +0900

    '20200201'

--- pix2pix_networks_20200111.py
+++ pix2pix_networks_20200111.py
@@ -68,4 +68,74 @@ class Discriminator(nn.Module):
 
 
 class Generator(nn.Module):
+    def __init__(self, size, idx_max_ch=None):
+        super(Generator, self).__init__()
+        act_down = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        act_up = nn.ReLU(inplace=True)
+        max_ch = 1024
+        n_downsample = int(log2(size))
+        n_gf = 64
+        norm = nn.BatchNorm2d
+        idx_max_ch = int(log2(max_ch // n_gf))
 
+        for i in range(n_downsample):
+            if i == 0:
+                down_block = [nn.Conv2d(1, n_gf, kernel_size=4, padding=1, stride=2, bias=False)]
+                up_block = [act_up,
+                            nn.ConvTranspose2d(2 * n_gf, 3, kernel_size=4, padding=1, stride=2, bias=False),
+                            nn.Tanh()]
+
+            elif 1 <= i <= idx_max_ch:
+                down_block = [act_down,
+                              nn.Conv2d(n_gf, 2 * n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                              norm(2 * n_gf)]
+                up_block = [act_up,
+                            nn.ConvTranspose2d(4 * n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                            norm(n_gf)]
+
+
+            elif idx_max_ch < i < n_downsample - 4:
+                down_block = [act_down,
+                              nn.Conv2d(n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                              norm(n_gf)]
+                up_block = [act_up,
+                            nn.ConvTranspose2d(2 * n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                            norm(n_gf)]
+
+            elif n_downsample - 4 <= i < n_downsample - 1:
+                down_block = [act_down,
+                              nn.Conv2d(n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                              norm(n_gf)]
+                up_block = [act_up,
+                            nn.ConvTranspose2d(2 * n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                            norm(n_gf),
+                            nn.Dropout2d(0.5, inplace=True)]
+
+            else:
+                down_block = [act_down,
+                              nn.Conv2d(n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False)]
+                up_block = [act_up,
+                            nn.ConvTranspose2d(n_gf, n_gf, kernel_size=4, padding=1, stride=2, bias=False),
+                            nn.Dropout2d(0.5, inplace=True)]
+
+            self.add_module("Down_block_{}".format(i), nn.Sequential(*down_block))
+            # add_module은 변수의 이름을 추가해주는 역할.
+            # Down_block_0 = nn.Sequential(*down_block) 이렇게 하는 역할.
+            self.add_module("Up_block_{}".format(i), nn.Sequential(*up_block))
+            n_gf *= 2 if n_gf < max_ch and i != 0 else 1
+
+        self.n_downsample = n_downsample
+
+    def forward(self, x):
+        layers = [x]
+        for i in range(self.n_downsample):
+            layers += [getattr(self, "Down_block_{}".format(i))(layers[-1])]
+        x = getattr(self, "Up_block_{}".format(self.n_downsample - 1))(layers[-1])
+
+
+        for i in range(self.n_downsample - 1, 0, -1):
+            x = getattr(self, "Up_block_{}".format(i - 1))(torch.cat([x, layers[i]], dim=1))
+
+        return x
+# 스킵 커넥션은 원본의 형상을 유지하기 위해서이다.
+# 앞쪽 단의 애를 뒷쪽 단의 애들과 연결해주면 gradient 가 path 를 따라 넘어가게 되는데,
--- pix2pix_pipeline_20200111.py
+++ pix2pix_pipeline_20200111.py
@@ -10,14 +10,18 @@ class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, dir_src, extension, is_train=True):
         super(CustomDataset, self).__init__()
         self.list_path_input = sorted(glob(os.path.join(dir_src, "Input", "*.{}".format(extension))))
+        # glob 는 같은확장자의 애들을 한번에 긁어오는 데 씀, 리스트로 만들어준다.
 
         if is_train:
-            self.list_path_input = sorted(glob(os.path.join(dir_src, "Target", "*.{}".format(extension))))
+            self.list_path_target = sorted(glob(os.path.join(dir_src, "Target", "*.{}".format(extension))))
             self.idx = random.randint(10)
 
         transform = [transforms.ToTensor(),
+                     # ToTensor pytorch가 인식할 수 있는 형태로 바꿔죽, 범위를 (0, 1) 로 바꿔주는 두가지 기능.
                      # transforms.RandomCrop(),  # 이걸 쓰면 타겟과 인풋이미지의 자르는 부분이 달라서 비교를 할 수 없게 된다.
                      transforms.Lambda(lambda x: self.__random_crop(x)),
+                     # random_crop 은 랜덤하게 이미지를 자르는 것? 이걸 씀으로써 적은 수의 이미지를 뻥튀기할 수 있다.
+
                      transforms.Normalize(mean=[0.5], std=[0.5])]
 
         self.transform = transforms.Compose(transform)
@@ -25,6 +29,7 @@ class CustomDataset(torch.utils.data.Dataset):
 
     def __random_crop(self, x, size=32):
         return x[self.idx:self.idx + size, self.idx:self.idx + size]
+        # 어디를 자를지 고르는 함수부분
 
     def __getitem__(self, index):
         input = Image.open(self.list_path_input[index])
